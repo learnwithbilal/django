f4eca681ef936f2822829ae1ef0b0042ee428a29
Bilal <learn.with.bilal@gmail.com>
2021-10-21 12:52:06 +0300

    add Django files

--- youtube/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("humam", views.humam, name="humam"),
-    path("ali", views.ali, name="ali"),
-    path("<str:name>", views.greet, name="greet")
-]
